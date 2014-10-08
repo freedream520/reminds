d562884b708519f4f4174ea07228f9f970cfed00
Ashin Chen <ashin.chen@bosondata.com.cn>
2014-10-08 15:51:13 +0800

    ..

--- reminds/admin.py
+++ reminds/admin.py
@@ -4,14 +4,23 @@ from django.contrib import admin
 from reminds.models import Remind
 from datetime import datetime
 
+
 def remind_date(obj):
     return datetime.strftime(obj.remind_date, '%m-%d %H:%M:%S')
 remind_date.short_description = '提醒日期'
+
 
 class RemindAdmin(admin.ModelAdmin):
     list_display = [
         remind_date, 'remind_text',
         'remind_email', 'remind_cycle'
     ]
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        obj.save()
+
+    def get_queryset(self, request):
+        return super(RemindAdmin, self).get_queryset(request).filter(user=request.user)
 
 admin.site.register(Remind, RemindAdmin)
--- reminds/models.py
+++ reminds/models.py
@@ -2,12 +2,14 @@
 from __future__ import unicode_literals
 from django.db import models
 from datetime import datetime
+from django.contrib.auth.models import User
 
 
 class Remind(models.Model):
     class Meta:
         verbose_name_plural = '我的提醒'
 
+    user = models.ForeignKey(User, editable=False, null=True, blank=True)
     remind_date = models.DateTimeField('提醒日期')
     remind_text = models.CharField('提醒信息', max_length=128)
     remind_email = models.EmailField('提醒邮箱')
@@ -15,10 +17,10 @@ class Remind(models.Model):
         '提醒周期',
         max_length=10,
         choices=(
-            ('everyyear', '每年'),
-            ('everymonth', '每月'),
-            ('everyweek', '每周'),
-            ('everyday', '每天'),
+            ('year', '每年'),
+            ('month', '每月'),
+            ('week', '每周'),
+            ('day', '每天'),
             ('once', '仅一次'),
         ),
         default='everyyear'
