f4fae243880996338cde365bcb61651d03bc908b
Ashin Chen <ashin.chen@bosondata.com.cn>
2014-10-08 11:30:36 +0800

    update verbose name

--- app/models.py
+++ app/models.py
@@ -3,9 +3,10 @@ from django.db import models
 
 class Remind(models.Model):
     remind_date = models.DateTimeField('Remind Date')
-    remind_text = models.TextField()
-    remind_email = models.EmailField()
+    remind_text = models.TextField('Remind Text')
+    remind_email = models.EmailField('Remind Email')
     remind_cycle = models.CharField(
+        'Remind Cycle',
         max_length=10,
         choices=(
             ('everyyear', 'everyyear'),
