30689fcd5b18a093bcfd40845173ad9930114453
Ashin Chen <ashin.chen@bosondata.com.cn>
2014-11-28 16:45:25 +0800

    ;fixed project name bug

--- main/wsgi.py
+++ main/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for date_remind project.
+WSGI config for reminds project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "date_remind.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "reminds.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
