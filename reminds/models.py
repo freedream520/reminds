3cf966601cb31e876f1990b125f80d1f5b8b6424
Ashin Chen <ashin.chen@bosondata.com.cn>
2014-12-01 10:50:01 +0800

    fixed decode bug

--- reminds/models.py
+++ reminds/models.py
@@ -1,5 +1,4 @@
 # -*- coding:utf-8 -*-
-from __future__ import unicode_literals
 from django.db import models
 from datetime import datetime
 from django.contrib.auth.models import User
@@ -9,26 +8,26 @@ import fileinput
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-cmd = '%s/.venv/bin/python %s/send_remind.py' % (BASE_DIR, BASE_DIR)
+cmd = u'%s/.venv/bin/python %s/send_remind.py' % (BASE_DIR, BASE_DIR)
 
 class Remind(models.Model):
 
     class Meta:
-        verbose_name_plural = '我的提醒'
+        verbose_name_plural = u'我的提醒'
 
-    user = models.ForeignKey(User, verbose_name='用户名', editable=False, null=True, blank=True)
-    remind_date = models.DateTimeField('提醒时间')
-    remind_text = models.CharField('提醒信息', max_length=128)
-    remind_email = models.EmailField('提醒邮箱')
+    user = models.ForeignKey(User, verbose_name=u'用户名', editable=False, null=True, blank=True)
+    remind_date = models.DateTimeField(u'提醒时间')
+    remind_text = models.CharField(u'提醒信息', max_length=128)
+    remind_email = models.EmailField(u'提醒邮箱')
     remind_cycle = models.CharField(
-        '提醒周期',
+        u'提醒周期',
         max_length=10,
         choices=(
-            ('yearly', '每年'),
-            ('monthly', '每月'),
-            ('weekly', '每周'),
-            ('daily', '每天'),
-            ('once', '仅一次'),
+            ('yearly', u'每年'),
+            ('monthly', u'每月'),
+            ('weekly', u'每周'),
+            ('daily', u'每天'),
+            ('once', u'仅一次'),
         ),
         default='yearly'
     )
@@ -47,24 +46,24 @@ class Remind(models.Model):
 
     def parse_cron(self, date, cycle):
         if cycle == 'once':
-            cron = '%s %s %s %s %s %s %s %s r_id%s' % (
+            cron = u'%s %s %s %s %s %s %s %s r_id%s' % (
                 date.minute, date.hour, date.day,
                 date.month, date.isoweekday(), cmd,
                 self.remind_email, self.remind_text, self.id)
         elif cycle == 'daily':
-            cron = '%s %s * * * %s %s %s r_id%s' % (
+            cron = u'%s %s * * * %s %s %s r_id%s' % (
                 date.minute, date.hour, cmd,
                 self.remind_email, self.remind_text, self.id)
         elif cycle == 'weekly':
-            cron = '%s %s * * %s %s %s %s r_id%s' % (
+            cron = u'%s %s * * %s %s %s %s r_id%s' % (
                 date.minute, date.hour, date.isoweekday(), cmd,
                 self.remind_email, self.remind_text, self.id)
         elif cycle == 'monthly':
-            cron = '%s %s %s * * %s %s %s r_id%s' % (
+            cron = u'%s %s %s * * %s %s %s r_id%s' % (
                 date.minute, date.hour, date.day, cmd,
                 self.remind_email, self.remind_text, self.id)
         elif cycle == 'yearly':
-            cron = '%s %s %s %s * %s %s %s r_id%s' % (
+            cron = u'%s %s %s %s * %s %s %s r_id%s' % (
                 date.minute, date.hour, date.day, date.month, cmd,
                 self.remind_email, self.remind_text, self.id)
         return cron
@@ -78,7 +77,7 @@ class Remind(models.Model):
             sys.stdout.write(line)
         if not is_update:
             with open('remind.cron', 'a+') as crontab:
-                crontab.write(cron.decode('utf-8') + '\n')
+                crontab.write(cron.encode('utf-8') + '\n')
         os.system('crontab remind.cron')
 
     def remove_cron(self, remind_id):
